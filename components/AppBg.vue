894eaa6a738dbd8dfb2a5f3c7a3227575aca1d2d
Ryan Bartlett <ryanzbartlett@gmail.com>
2024-04-28 16:47:24 -0400

    Final cleanup

---
+++ components/AppBg.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+const props = defineProps<{
+  isOffAir: boolean;
+  currentTrack: Jamfluencer.Track;
+}>();
+
+const OFF_AIR_BG = '/images/test_pattern.png';
+
+const bgArt = computed(() => props.currentTrack.bg);
+const bgFrom = ref<HTMLImageElement | null>(null);
+const bgTo = ref<HTMLImageElement | null>(null);
+
+watch(props.currentTrack, onTrackChange, { immediate: true });
+
+async function onTrackChange(nextTrack: Jamfluencer.Track) {
+  if (!bgFrom.value || !bgTo.value) return;
+
+  const bgFromEl = bgFrom.value;
+  const bgToEl = bgTo.value;
+
+  bgToEl.src = nextTrack.bg;
+
+  bgFromEl.classList.add('fade-out');
+  bgToEl.classList.add('fade-in');
+
+  await new Promise<void>((resolve) => {
+    bgToEl.addEventListener('animationend', () => {
+      bgFromEl.src = nextTrack.bg;
+      bgFromEl.classList.remove('fade-out');
+      bgToEl.classList.remove('fade-in');
+      resolve();
+    });
+  });
+}
+</script>
+
+<template>
+  <div id="background" class="fixed top-0 left-0 w-full h-full">
+    <div v-if="isOffAir" class="w-full h-full">
+      <img
+        class="absolute w-full h-full object-cover"
+        :src="OFF_AIR_BG"
+        alt=""
+      />
+    </div>
+    <div v-else class="w-full h-full">
+      <img
+        ref="bgFrom"
+        class="absolute w-full h-full object-cover"
+        :src="bgArt"
+        alt=""
+      />
+      <img
+        ref="bgTo"
+        class="absolute w-full h-full object-cover"
+        src=""
+        alt=""
+        style="opacity: 0"
+      />
+    </div>
+    <div
+      class="absolute top-0 left-0 w-full h-full bg-black"
+      :class="{
+        'opacity-45': !isOffAir,
+        'opacity-85': isOffAir,
+      }"
+    />
+  </div>
+</template>
+
+<style>
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+@keyframes fadeOut {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+}
+
+.fade-in {
+  animation: fadeIn 1s;
+}
+
+.fade-out {
+  animation: fadeOut 1s;
+}
+</style>
---
+++ components/TrackDisplay.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  track: Jamfluencer.Track;
+}>();
+</script>
+
+<template>
+  <div class="flex-auto flex gap-6 items-end">
+    <img class="w-32 h-32 rounded" :src="track.album" alt="current" />
+    <div class="flex flex-col gap-1">
+      <UserTag :user="track.addedBy" class="mb-2" />
+      <div class="text-4xl font-bold">
+        {{ track.title }}
+      </div>
+      <div>
+        {{ track.artist }}
+      </div>
+    </div>
+  </div>
+</template>
