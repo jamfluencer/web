34a8c1c8baf667104e02c39af44d0ff228bef2d8
Ryan Bartlett <ryanzbartlett@gmail.com>
2024-05-03 08:26:28 -0400

    Add about text

--- components/AppAboutDrawer.vue
+++ components/AppAboutDrawer.vue
@@ -5,6 +5,17 @@ const isOpen = defineModel<boolean>({ required: true });
 <template>
   <CommonDrawer v-model="isOpen">
     <template #title>About</template>
-    <p>Jamfluencer.app is...</p>
+    <p>
+      <span class="text-primary">jamfluencer.app</span> is a companion app for
+      the weekly Influencer Jam.
+    </p>
+    <p class="mt-4">
+      While the Jam is in progress, throw this in a window nearby for a fun,
+      visual accompaniment while you enjoy the choices of your colleagues.
+    </p>
+    <p class="mt-4">
+      We hope to add more features in the future, so check back each week!
+    </p>
+    <p class="mt-4">jamfluencer.app is a project by Ryan and Marshall.</p>
   </CommonDrawer>
 </template>
