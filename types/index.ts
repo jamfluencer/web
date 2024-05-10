359cef1c4737bf0cad5a7e5997286438c3dab67b
Ryan Bartlett <ryanzbartlett@gmail.com>
2024-05-10 08:19:42 -0400

    Cleanup

--- types/index.ts
+++ types/index.ts
@@ -1,21 +1,7 @@
 export {};
 
 declare global {
-  namespace Jamfluencer {
-    interface Track {
-      id: string;
-      bg: string;
-      album: string;
-      title: string;
-      artist: string;
-      addedBy: string;
-    }
-
-    interface Playlist {
-      name: string;
-      tracks: Track[];
-    }
-  }
+  namespace Jamfluencer {}
 
   namespace JamfluencerApi {
     interface User {
