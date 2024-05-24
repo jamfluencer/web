4b84d061a43c5aaab97c229c8e6839ba596147ca
Ryan Bartlett <ryanzbartlett@gmail.com>
2024-05-23 21:20:48 -0400

    Include Spotify attribution

--- components/AppAboutDrawer.vue
+++ components/AppAboutDrawer.vue
@@ -5,17 +5,31 @@ const isOpen = defineModel<boolean>({ required: true });
 <template>
   <CommonDrawer v-model="isOpen">
     <template #title>About</template>
-    <p>
-      <span class="text-primary">jamfluencer.app</span> is a companion app for
-      the weekly Influencer Jam.
-    </p>
-    <p class="mt-4">
-      While the Jam is in progress, throw this in a window nearby for a fun,
-      visual accompaniment while you enjoy the choices of your colleagues.
-    </p>
-    <p class="mt-4">
-      We hope to add more features in the future, so check back each week!
-    </p>
-    <p class="mt-4">jamfluencer.app is a project by Ryan and Marshall.</p>
+    <div class="flex flex-col gap-4">
+      <p>
+        <span class="text-primary">jamfluencer.app</span> is a companion app for
+        the weekly Influencer Jam.
+      </p>
+      <p>
+        While the Jam is in progress, throw this in a window nearby for a fun,
+        visual accompaniment while you enjoy the choices of your colleagues.
+      </p>
+      <p>
+        We hope to add more features in the future, so check back each week!
+      </p>
+      <p>jamfluencer.app is a project by Ryan and Marshall.</p>
+      <p>All track metadata provided by Spotify.</p>
+      <a
+        href="https://www.spotify.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="/assets/images/spotify_logo_with_text.svg"
+          alt="Spotify Logo"
+          class="w-full max-w-[10rem]"
+        />
+      </a>
+    </div>
   </CommonDrawer>
 </template>
