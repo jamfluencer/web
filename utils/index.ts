79c082c9b892511e5c055efcb9098263fbb04c3e
Ryan Bartlett <ryanzbartlett@gmail.com>
2024-06-11 20:41:56 -0400

    Add names for new contributors 🎉

--- utils/index.ts
+++ utils/index.ts
@@ -2,12 +2,16 @@ export const userMap: Record<string, string> = {
   '1236711152': 'James',
   'matt.witkowicz': 'Matt',
   '31yyjf7lal2fpxw7umyo4abj7boy': 'Matt',
-  hzcvr820nvkv5mwamvptxhns4: 'Nate',
-  i88pa9ajt0fhh089bz7vhwd48: 'Nick',
-  prince_of_space: 'Ryan',
-  toothlessrebel: 'Marshall',
+  'hzcvr820nvkv5mwamvptxhns4': 'Nate',
+  'i88pa9ajt0fhh089bz7vhwd48': 'Nick',
+  'prince_of_space': 'Ryan',
+  'toothlessrebel': 'Marshall',
   '31jx2bfbged6bjpaevna4xkxjhem': 'Scott',
   '12158652399': 'Stewan',
+  'csdvx92jpnz6n418mx27syaox': 'Ricardo',
+  'musicaljoeker': 'Joseph',
+  '31u5udr3hglumfasdxkce66mvani': 'Kalyn',
+  '21xxwycxix6fo62h6akfpz6oi': 'Wade',
 } as const;
 
 export const getUser = (id: string): string => userMap[id] ?? id;
