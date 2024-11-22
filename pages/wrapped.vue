6ecec1e5a7cf39b9bbc34fcde00b58c6d481f821
Ryan Bartlett <ryanzbartlett@gmail.com>
2024-11-21 23:05:36 -0500

    Wrapped title exploration

---
+++ pages/wrapped.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import gsap from 'gsap';
+
+const titleLetters = 'WRAPPED!'.split('');
+
+onMounted(() => {
+  let tl = gsap.timeline();
+
+  tl.from(
+    titleLetters.map((_, index) => `#wrapped-letter-${index}`),
+    {
+      duration: 0.5,
+      x: 0,
+      y: -100,
+      opacity: 0,
+      ease: 'bounce.out',
+      stagger: 0.2,
+      delay: 0.5,
+    }
+  );
+});
+</script>
+
+<template>
+  <div class="h-screen w-full flex items-center justify-center cursor-default">
+    <div class="flex">
+      <div
+        :id="`wrapped-letter-${index}`"
+        v-for="(letter, index) in titleLetters"
+        :key="index"
+        class="font-wrapped text-8xl"
+      >
+        {{ letter }}
+      </div>
+    </div>
+  </div>
+</template>
