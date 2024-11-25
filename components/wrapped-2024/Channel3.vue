a626cc93c8a7e6a183485dbde0fb3ff65b0902bc
Ryan Bartlett <ryanzbartlett@gmail.com>
2024-11-24 21:43:05 -0500

    WIP - Wrapped TV

---
+++ components/wrapped-2024/Channel3.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const props = defineProps<{
+  active: boolean;
+}>();
+
+const video = ref<HTMLVideoElement | null>(null);
+
+function onChannelActive() {
+  video.value?.play();
+}
+
+function onChannelInactive() {}
+
+watch(
+  () => props.active,
+  (active) => {
+    if (active) {
+      onChannelActive();
+    } else {
+      onChannelInactive();
+    }
+  }
+);
+</script>
+
+<template>
+  <video
+    ref="video"
+    src="~/assets/wrapped2024/dump_cakes.mp4"
+    class="w-full h-full object-cover"
+    loop
+  />
+</template>
